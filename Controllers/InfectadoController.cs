f8aa87bd3c21e2c564807fa2d38fbd85caba6bb0
DAYVSON SILVINO DA SILVA <62804086+21121990@users.noreply.github.com>
2021-05-18 20:07:01 -0300

    put and delete

--- Controllers/InfectadoController.cs
+++ Controllers/InfectadoController.cs
@@ -40,5 +40,21 @@ namespace Api.Controllers
             var infectados = _infectadosCollection.Find(Builders<infectado>.Filter.Empty).ToList();
             return Ok(infectados);
         }
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+
+            var infectados = new infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+           _infectadosCollection.UpdateOne(Builders<infectado>.Filter.Where(x => x.DataNascimento == dto.DataNascimento),Builders<infectado>.Update.Set("sexo", dto.Sexo));
+            return Ok("Atualizado com sucesso");
+        }
+
+        [HttpDelete("{dtn}")]
+        public ActionResult Delete(DateTime dtn)
+        {
+
+           _infectadosCollection.DeleteOne(Builders<infectado>.Filter.Where(x => x.DataNascimento == dtn));
+            return Ok("Cadastro excluído");
+        }
     }
 }
